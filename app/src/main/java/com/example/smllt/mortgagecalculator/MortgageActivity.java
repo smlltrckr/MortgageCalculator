5b2413895fc57027d56c698572b53f044282e07b
Sam Rucker <Sam Rucker>
2016-04-25 21:59:05 -0700

    GUI updates and added some TODOs

--- app/src/main/java/com/example/smllt/mortgagecalculator/MortgageActivity.java
+++ app/src/main/java/com/example/smllt/mortgagecalculator/MortgageActivity.java
@@ -1,51 +1,55 @@
 package com.example.smllt.mortgagecalculator;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class MortgageActivity extends AppCompatActivity {
+    private Button calculateBtn, resetBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mortgage);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        // Loads up the choices for the terms spinner.
+        Spinner termsDropdown = (Spinner)findViewById(R.id.terms);
+        String[] termsArray = new String[]{"", "15", "20", "25", "30", "40"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, termsArray);
+        termsDropdown.setAdapter(adapter);
+
+        calculateBtn = (Button) findViewById(R.id.calculate);
+        resetBtn = (Button) findViewById(R.id.reset);
+
+        calculateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO:
+            }
+        });
+
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO:
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_mortgage, menu);
-        return true;
+    /**
+     * Does all the calculations when finished sets Output LinearLayout to Visible
+     */
+    private void calculate() {
+        //TODO:
     }
 
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
+    /**
+     *  Clears all the fields and sets Output LinearLayout to invisible
+     */
+    private void reset(){
+        //TODO:
+    }
 }
